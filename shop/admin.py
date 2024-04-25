061fb4c271a561d10b643feeba894f4d1ac3094f
piseysen <piseysen@piseys-MacBook-Pro.local>
2024-04-25 15:35:35 +0700

    fix bug

--- shop/admin.py
+++ shop/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import Products, Order
 
 # Register your models here.
-admin.site.register(Products)
-admin.site.register(Order)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'discount_price', 'category', 'image']
+    
+admin.site.register(Products, ProductsAdmin)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['items', 'name', 'email', 'address', 'city', 'state', 'zipcode', 'total']
+admin.site.register(Order, OrderAdmin)
